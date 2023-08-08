95aa21f547f9a5bc8ee544d4d868660ff983d7e0
Bo.Jin <bo.jin@borgor.cn>
2023-08-08 22:40:07 +0800

    change all env to configs

--- src/configs/config.dev.ts
+++ src/configs/config.dev.ts
@@ -2,4 +2,8 @@ import { type ConfigType } from "@/configs/configType";
 
 export const devConfig: Partial<ConfigType> = {
   isDev: true,
+  server: {
+    bind: "0.0.0.0",
+    port: 8000,
+  },
 };
